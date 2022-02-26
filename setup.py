d6111054cc30af2a80a18c6f57db7e7ebb3855e0
fire1ce <stas@3os.org>
2022-02-26 14:02:32 +0200

    1.1.0 release

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="mkdocs-embed-external-markdown",
-    version="1.0.1",
+    version="1.1.0",
     url="https://github.com/fire1ce/mkdocs-embed-external-markdown",
     license="MIT",
     author="Stas Yakobov",
