3cb7af1bf44c2915b041b34dc2f007951a593cbe
Stas Yakobov <stas@3os.org>
2023-06-21 01:43:02 +0300

    plugin rewrite

--- external_markdown/plugin.py
+++ external_markdown/plugin.py
@@ -1,77 +1,145 @@
-from mkdocs.plugins import BasePlugin
-from requests import get, exceptions
-from re import compile, match, MULTILINE
-from jinja2 import Template
-from sys import exit
 import os
+import logging
+import re
+from mkdocs.plugins import BasePlugin
+from requests import get, ConnectionError, Response
+from jinja2 import Template
+from urllib.parse import urljoin
+from typing import Optional
+
+# Pre-compile regular expressions
+SECTION_LEVEL_REGEX = re.compile("^#+ ", re.IGNORECASE)
+LINK_PATTERN_REGEX = re.compile(r"\[(?P<alt_text>[^\]]*)\]\((?P<link_url>[^\)]*)\)", re.MULTILINE | re.IGNORECASE)
+
+logger = logging.getLogger("mkdocs.plugins")
+
 
 class EmbedExternalMarkdown(BasePlugin):
+    """
+    A MkDocs plugin to embed external Markdown content into the documentation.
+    """
 
-    # check if url is valid and had ".md" extension
-    def is_valid_url(self, url):
-        if not match(r"^https?:\/\/.*\.md$", url):
-            print(f"Error! {url} is not a valid url")
-            exit(1)
-        return True
+    def is_valid_url(self, url: str) -> bool:
+        """
+        Check if the provided URL is valid and is a markdown file.
+        """
+        # Regex pattern to validate URLs
+        pattern = re.compile(
+            r"http[s]?://"  # http:// or https://
+            r"(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?|"  # domain...
+            r"localhost|"  # localhost...
+            r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"  # ...or ip
+            r"(?::\d+)?"  # optional port
+            r"(?:/?|[/?]\S+)$",
+            re.IGNORECASE,
+        )
 
-    # get markdown from url if status code is 200
-    def get_markdown_from_url(self, url):
-        try:
-            GH_TOKEN = os.getenv('GH_TOKEN')
-            if GH_TOKEN:
-                headers = {'Authorization': 'token ' + GH_TOKEN}
-                response = get(url, headers=headers)
-            else: 
-                response = get(url)
-            if response.status_code == 200:
-                # remove the heading
-                markdown = response.text
-                markdown = markdown[markdown.find("\n") + 1 :]
-                return markdown
-            else:
-                print(f"Error! {url} returned status code: {str(response.status_code)}")
-                exit(1)
-        except exceptions.ConnectionError:
-            print(f"Error! {url} returned connection error")
-            exit(1)
+        # Check if URL is valid and ends with .md
+        if re.match(pattern, url) and url.lower().endswith(".md"):
+            return True
+        else:
+            logger.warning(f"{url} is not a valid markdown URL")
+            return False
 
-    # get the section content from markdown
-    def get_section_from_markdown(self, markdown, section_name, url):
-        # Get the section level from section_name
+    def make_request(self, url: str) -> Optional[Response]:
+        """
+        Make a GET request to the specified URL and return the response.
+        """
         try:
-            section_level = compile("^#+ ").search(section_name).span()[1] - 1
-        except:
-            print(
-                f"Error! Missing markdown section level at the beginning of section name: {section_name}"
-            )
-            exit(1)
-        # Gets the srart index of the section from markdown
+            headers = {}
+            gh_token = os.getenv("GH_TOKEN")
+            if gh_token:
+                headers = {"Authorization": "token " + gh_token}
+
+            return get(url, headers=headers)
+        except ConnectionError:
+            logger.warning(f"{url} returned connection error")
+            return None
+
+    def get_markdown_from_response(self, response: Response, url: str) -> Optional[str]:
+        """
+        Extract markdown text from the response object.
+        """
+        if response.status_code == 200:
+            markdown = response.text
+            return markdown[markdown.find("\n") + 1 :]
+        else:
+            logger.warning(f"{url} returned status code: {str(response.status_code)}")
+            return None
+
+    def get_section_level(self, section_name: str) -> Optional[int]:
+        """
+        Get the level of the section based on markdown headers (e.g., ##, ###, etc.)
+        """
         try:
-            start_index = compile("^" + section_name + "$", MULTILINE).search(markdown).span()[1]
-        except:
-            print(f'Error! Section: "{section_name}" not found in markdown {url}')
-            exit(1)
-        # Gets the end index of the section from markdown (last section handle)
+            return SECTION_LEVEL_REGEX.search(section_name).span()[1] - 1
+        except AttributeError:
+            logger.warning(f"Missing markdown section level at the beginning of section name: {section_name}")
+            return None
+
+    def extract_section_from_markdown(
+        self, markdown: str, section_name: str, section_level: int, url: str
+    ) -> Optional[str]:
+        """
+        Extract a specific section from the markdown text.
+        """
+        try:
+            section_pattern = f"^{section_name.strip()}(?:[^#]|$)"
+            start_index = re.compile(section_pattern, re.MULTILINE | re.IGNORECASE).search(markdown).span()[1]
+        except AttributeError:
+            logger.warning(f'Section: "{section_name}" not found in markdown {url}')
+            return None
+
         try:
             end_index = (
-                compile("^#{2," + str(section_level) + "} ", MULTILINE)
+                re.compile("^#{2," + str(section_level) + "} ", re.MULTILINE | re.IGNORECASE)
                 .search(markdown[start_index:])
                 .span()[0]
             )
-            markdown = markdown[start_index : end_index + start_index]
-        except:
-            markdown = markdown[start_index:]
+            return markdown[start_index : end_index + start_index]
+        except AttributeError:
+            return markdown[start_index:]
+
+    def update_relative_links(self, markdown: str, base_url: str) -> str:
+        """
+        Update relative links in markdown to absolute links.
+        """
+
+        def replace_link(match):
+            link_url = urljoin(base_url, match.group("link_url"))
+            return f'[{match.group("alt_text")}]({link_url})'
+
+        return LINK_PATTERN_REGEX.sub(replace_link, markdown)
+
+    def external_markdown(self, url: str, section_name: Optional[str] = None) -> str:
+        """
+        Retrieve and process external markdown content from the specified URL.
+        Optionally extract a section if section_name is provided.
+        """
+        if not self.is_valid_url(url):
+            return ""
+
+        response = self.make_request(url)
+        if response is None:
+            return ""
+
+        markdown = self.get_markdown_from_response(response, url)
+        if markdown is None:
+            return ""
+
+        markdown = self.update_relative_links(markdown, url)
+
+        if section_name:
+            section_level = self.get_section_level(section_name)
+            if section_level is not None:
+                markdown = self.extract_section_from_markdown(markdown, section_name, section_level, url) or ""
+            else:
+                markdown = ""
+
         return markdown
 
-    # get the markdown from url or markdown section
-    def external_markdown(self, url, section_name):
-        self.is_valid_url(url)
-        if section_name:
-            markdown = self.get_markdown_from_url(url)
-            if markdown:
-                return self.get_section_from_markdown(markdown, section_name, url)
-        else:
-            return self.get_markdown_from_url(url)
-
-    def on_page_markdown(self, markdown, **kwargs):
+    def on_page_markdown(self, markdown: str, **kwargs) -> str:
+        """
+        Render the markdown content using the Jinja2 template engine.
+        """
         return Template(markdown).render(external_markdown=self.external_markdown)
